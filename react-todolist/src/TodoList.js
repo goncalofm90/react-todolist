6102ac13f89d8806168b284898ada45df49838e6
goncalofm90 <goncalofm90@gmail.com>
2020-10-22 16:36:18 +0100

    finished exercise

---
+++ react-todolist/src/TodoList.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import NewTodoForm from './NewTodoForm';
+import Todo from './Todo';
+import './TodoList.css'
+
+
+export class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {todos: []}
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.toggleCompletion = this.toggleCompletion.bind(this);
+
+  }update
+  remove(id){
+    this.setState({
+      todos: this.state.todos.filter(t => t.id !== id)
+    });
+  }
+  update(id, updatedTask){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, task: updatedTask}
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos})
+  }
+  create(newTodo){
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+  toggleCompletion(id){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, completed: !todo.completed}
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos})
+  }
+  render() {
+    const todos = this.state.todos.map(todo => {
+      return <Todo 
+      key={todo.id} 
+      id={todo.id}
+      task={todo.task} 
+      completed={todo.completed}
+      removeTodo={this.remove}
+      updateTodo={this.update}
+      toggleTodo={this.toggleCompletion}
+      />
+    })
+    return (
+      <div className="TodoList">
+        <h1>Todo List <span>A simple react Todo List App.</span></h1>
+        <ul>{todos}</ul>
+        <NewTodoForm createTodo={this.create}/>
+      </div>
+    )
+  }
+}
+
+export default TodoList
